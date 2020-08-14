bd0b12c82f455c667c6517e93eb3bb0054d923d5
Roberto Livi <60152068+Roberto-Livi@users.noreply.github.com>
2020-08-13 21:15:19 -0400

    Add files via upload

---
+++ src/components/Accordion.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const Accordion = ({items}) => {
+
+    const [activeIndex, setActiveindex] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveindex(index);
+    }
+
+    // en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=SEARCHTERM
+
+    const renderedItems = items.map((item, index) => {
+
+        const active = index === activeIndex ? "active" : ''
+
+        return <React.Fragment key={item.title}>
+            <div 
+            className={`title ${active}`}
+            onClick={() => onTitleClick(index)}
+            >
+                <i className="dropdown icon"></i>
+                {item.title}
+            </div>
+            <div className={`content ${active}`}>
+                <p>{item.content}</p>
+            </div>
+        </React.Fragment>
+    })
+
+    return <div className=" ui styled accordion">
+        {renderedItems}
+    </div>
+}
+
+export default Accordion
